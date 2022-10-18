6b06068c04178faa6769cd6efb26ce46a56b0739
Felix Roos <roos@entrecode.de>
2022-10-18 09:39:16 +0200

    library build + gh packages config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
+import { peerDependencies, dependencies } from './package.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +11,16 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src', 'index.ts'),
+      formats: ['es', 'cjs'],
+      fileName: (ext) => `index.${ext}.js`,
+      // for UMD name: 'GlobalName'
+    },
+    rollupOptions: {
+      external: [...Object.keys(peerDependencies), ...Object.keys(dependencies)],
+    },
+    target: 'esnext',
+  },
 });
