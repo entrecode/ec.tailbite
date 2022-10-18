6ad29120296c96e022052e5b48ab4c7d645ee7dd
Felix Roos <roos@entrecode.de>
2022-10-18 16:53:53 +0200

    fix api provider

--- src/hooks/useSdk.tsx
+++ src/hooks/useSdk.tsx
@@ -1,13 +1,12 @@
 import { PublicAPI, Session } from 'ec.sdk';
 import { environment as Environment, options as Options } from 'ec.sdk/lib/Core';
-import { useCallback, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { createContext, useCallback, useContext, useEffect } from 'react';
 import useSWRImmutable from 'swr/immutable';
 import environment from '../environment';
 import useAccounts from './useAccounts';
 
 export interface UsePublicAPIProps {
-  shortID: string;
+  shortID: string | null;
   env?: Options | Environment;
   swrOptions?: any;
 }
@@ -16,7 +15,7 @@ export function usePublicAPI(props: UsePublicAPIProps) {
   const { shortID, env, swrOptions } = props;
   const res = useSWRImmutable(
     shortID ? [shortID, env] : null,
-    () => new PublicAPI(shortID, env, true).resolve(),
+    () => new PublicAPI(shortID!, env, true).resolve(),
     swrOptions,
   );
   useEffect(() => {
@@ -28,11 +27,22 @@ export function usePublicAPI(props: UsePublicAPIProps) {
   return res;
 }
 
-/* export const PublicAPIProvider = (props: any) => {
+const PublicAPIContext = createContext<PublicAPI | undefined>(undefined);
+
+export const PublicAPIProvider = (props: any) => {
   const { children, shortID, env = environment.env } = props;
-  const { data: api } = usePublicAPI({ shortID, env });
+  const { data: api } = usePublicAPI({
+    shortID,
+    env,
+    swrOptions: {
+      retryOnError: false,
+      onError: (err) => {
+        console.warn(`error loading public api with shortID "${shortID}"`, err);
+      },
+    },
+  });
   return <PublicAPIContext.Provider value={api}>{children}</PublicAPIContext.Provider>;
-}; */
+};
 
 export function useSession() {
   const res = useSWRImmutable(['Session', environment.env], () => {
@@ -51,20 +61,7 @@ export function useSession() {
  * ```
  */
 function useSdk() {
-  const { pathname } = useLocation(); // make sure useSdk is always called inside BrowserRouter
-  const shortID = pathname?.split('/')?.[1]?.match(/^[a-f0-9]{8}$/i) ? pathname.split('/')[1] : '';
-
-  const { data: api, error } = usePublicAPI({
-    shortID,
-    env: environment.env,
-    swrOptions: {
-      retryOnError: false,
-      onError: (err) => {
-        console.warn(`error loading public api with shortID "${shortID}"`, err);
-      },
-    },
-  });
-
+  const api = useContext(PublicAPIContext);
   const apiResolved = !!api;
   const { data: accounts } = useAccounts();
   const { data: session, mutate, token } = useSession();
