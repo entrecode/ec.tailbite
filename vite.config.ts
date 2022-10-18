295e91911a2d712e1a8d81165be7e93442819bbe
Felix Roos <roos@entrecode.de>
2022-10-18 10:59:41 +0200

    add vite-plugin-dts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { peerDependencies, dependencies } from './package.json';
+import dts from 'vite-plugin-dts';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,7 +11,12 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src/'),
     },
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src', 'index.ts'),
