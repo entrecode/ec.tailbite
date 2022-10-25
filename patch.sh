45b5ad02c1e4ba0e735f0e59c0375cada7ca940b
Felix Roos <roos@entrecode.de>
2022-10-25 11:49:53 +0200

    copy to dist after bump

--- patch.sh
+++ patch.sh
@@ -4,11 +4,11 @@ git diff-index --quiet HEAD --
 git checkout develop
 git pull
 npm run build && 
-cp package.json dist &&
-cp README.md dist &&
 bump patch && 
 npm i --force && 
 git add . && 
 git commit -m "bump version" &&
+cp package.json dist &&
+cp README.md dist &&
 cd dist &&
 npm publish
