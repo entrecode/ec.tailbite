99bb477643e5fa471e650bd7bfad4f7b846a463e
Felix Roos <roos@entrecode.de>
2022-10-25 11:47:50 +0200

    publish from dist

--- patch.sh
+++ patch.sh
@@ -4,8 +4,11 @@ git diff-index --quiet HEAD --
 git checkout develop
 git pull
 npm run build && 
+cp package.json dist &&
+cp README.md dist &&
 bump patch && 
 npm i --force && 
 git add . && 
 git commit -m "bump version" &&
+cd dist &&
 npm publish
