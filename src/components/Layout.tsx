dabf278a535c1524c16f5ac4a455d281562928a6
Felix Roos <roos@entrecode.de>
2022-12-13 12:56:51 +0100

    can now add extra classes to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,22 +1,38 @@
 import React, { FC, PropsWithChildren } from 'react';
+import cx from '@/util/classNames';
 
-const Layout: FC<PropsWithChildren<{ top?: any; left?: any; right?: any }>> = ({ top, left, right, children }) => {
+const Layout: FC<PropsWithChildren<any>> = ({ top, left, right, children, topClass, leftClass, rightClass }) => {
   return (
     <div className="flex flex-col h-screen">
       {top && (
-        <div className="flex-none h-16 items-center flex shadow-md p-1 z-[10] border-gray-300 dark:border-gray-700 border-b">
+        <div
+          className={cx(
+            'flex-none h-16 items-center flex shadow-md p-1 z-[10] border-gray-300 dark:border-gray-700 border-b',
+            topClass,
+          )}
+        >
           {top}
         </div>
       )}
       <div className="flex grow overflow-hidden z-[11] ">
         {left && (
-          <div className="overflow-auto h-full bg-base border-r border-gray-300 dark:border-gray-700 shadow-lg">
+          <div
+            className={cx(
+              'overflow-auto h-full bg-base border-r border-gray-300 dark:border-gray-700 shadow-lg',
+              leftClass,
+            )}
+          >
             {left}
           </div>
         )}
         <div className="grow h-full overflow-auto">{children}</div>
         {right && (
-          <div className="overflow-auto h-full bg-base border-r border-gray-300 dark:border-gray-700 shadow-lg">
+          <div
+            className={cx(
+              'overflow-auto h-full bg-base border-r border-gray-300 dark:border-gray-700 shadow-lg',
+              rightClass,
+            )}
+          >
             {right}
           </div>
         )}
