8d1d84248edd2e1d3a23806149cf49e7fd4e53a7
Felix Roos <roos@entrecode.de>
2022-12-13 12:36:45 +0100

    layout: can now omit top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,9 +3,11 @@ import React, { FC, PropsWithChildren } from 'react';
 const Layout: FC<PropsWithChildren<{ top?: any; left?: any; right?: any }>> = ({ top, left, right, children }) => {
   return (
     <div className="flex flex-col h-screen">
-      <div className="flex-none h-16 items-center flex shadow-md p-1 z-[10] border-gray-300 dark:border-gray-700 border-b">
-        {top}
-      </div>
+      {top && (
+        <div className="flex-none h-16 items-center flex shadow-md p-1 z-[10] border-gray-300 dark:border-gray-700 border-b">
+          {top}
+        </div>
+      )}
       <div className="flex grow overflow-hidden z-[11] ">
         {left && (
           <div className="overflow-auto h-full bg-base border-r border-gray-300 dark:border-gray-700 shadow-lg">
