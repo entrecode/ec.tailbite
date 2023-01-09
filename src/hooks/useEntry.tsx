1cf0e3b5dcf06db55ce54ffee77f5bcd7d111652
Felix Roos <roos@entrecode.de>
2023-01-09 17:17:08 +0100

    useEntry optional params

--- src/hooks/useEntry.tsx
+++ src/hooks/useEntry.tsx
@@ -3,14 +3,14 @@ import useSWR, { SWRConfiguration, SWRResponse } from 'swr';
 import useSdk from './useSdk';
 
 export interface UseEntryProps {
-  model: string;
-  id: string;
+  model: string | null;
+  id: string | null;
   swrOptions?: SWRConfiguration<EntryResource, any>; // TODO: add error typing
 }
 
 function useEntry(props: UseEntryProps): SWRResponse<EntryResource, any> {
   const { model, id, swrOptions } = props;
   const { api } = useSdk();
-  return useSWR(api && model && id ? ['useEntry', model, id] : null, () => api!.entry(model, id), swrOptions);
+  return useSWR(api && model && id ? ['useEntry', model, id] : null, () => api!.entry(model!, id!), swrOptions);
 }
 export default useEntry;
