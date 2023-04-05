74350100ff2ac9a935889cb31f0358d6bf2643fe
Felix Roos <roos@entrecode.de>
2023-04-05 12:17:31 +0200

    fix: OptionsMenu - dont break on whitspace

--- src/components/OptionsMenu.tsx
+++ src/components/OptionsMenu.tsx
@@ -68,7 +68,7 @@ export const OptionsMenuItem = ({
       <a
         onClick={onClick}
         className={classNames(
-          'block px-4 py-2 text-sm text-gray-800 dark:text-gray-100 cursor-pointer',
+          'block px-4 py-2 text-sm text-gray-800 dark:text-gray-100 cursor-pointer whitespace-nowrap',
           active ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100' : '',
           className,
         )}
