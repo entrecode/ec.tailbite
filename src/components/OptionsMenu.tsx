02bef8fa78f3f916c7467268fdecf8343eab1b17
Felix Roos <roos@entrecode.de>
2023-04-05 12:08:42 +0200

    fix: OptionsMenu remove fixed width + use pointy cursor

--- src/components/OptionsMenu.tsx
+++ src/components/OptionsMenu.tsx
@@ -21,7 +21,7 @@ const OptionsMenu: any = ({
       <div>
         <Menu.Button as="div">
           {button || (
-            <div className="flex items-center focus:outline-none px-2 py-1">
+            <div className="flex items-center focus:outline-none px-2 py-1 cursor-pointer">
               <Ink.Primary>
                 <span className="sr-only">Open options</span>
                 <EllipsisVerticalIcon className="h-5 w-5" aria-hidden="true" />
@@ -43,7 +43,7 @@ const OptionsMenu: any = ({
         <Menu.Items
           className={classNames(
             $right ? 'origin-top-right right-0' : 'origin-top-left left-0',
-            'z-[31] absolute mt-2 w-56 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none',
+            'z-[31] absolute mt-2 rounded-md shadow-lg bg-white dark:bg-gray-800 ring-1 ring-black ring-opacity-5 focus:outline-none',
             dropdownClasses,
           )}
         >
@@ -68,7 +68,7 @@ export const OptionsMenuItem = ({
       <a
         onClick={onClick}
         className={classNames(
-          'block px-4 py-2 text-sm text-gray-800 dark:text-gray-100',
+          'block px-4 py-2 text-sm text-gray-800 dark:text-gray-100 cursor-pointer',
           active ? 'bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100' : '',
           className,
         )}
